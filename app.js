8c243d9619754eb5079a131b5331fa97147cc918
Vlad <astr0xy1@gmail.com>
2020-01-18 17:57:05 +0300

    changed project structure

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const fs = require('fs');
+const path = require('path');
 const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const config = require('config');
+
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const download = (url, filename, callback) => {
   request.head(url, () => {
@@ -33,6 +45,8 @@ app.post('/', (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log('Server is started!');
+const PORT = config.get('PORT') || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server has been started on ${PORT}!`);
 });
